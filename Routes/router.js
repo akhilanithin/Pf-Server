feec6efae54da12ac77aa809155764893d5c72cf
soniagkalliath <soniageorge111@gmail.com>
2023-11-20 13:00:51 +0530

    get projects logic

--- Routes/router.js
+++ Routes/router.js
@@ -3,12 +3,20 @@ const router = new express.Router()
 const userController = require('../Controllers/userController')
 const projectController = require('../Controllers/projectController')
 const jwtMiddleware = require('../Middlewares/jwtMiddleware')
+const multerConfig = require('../Middlewares/multerMiddleware')
 
 //register API
 router.post('/user/register',userController.register)
 //login
 router.post('/user/login',userController.login)
 //add-project
-router.post('/project/add',jwtMiddleware,projectController.addProjects)
+router.post('/project/add',jwtMiddleware,multerConfig.single('projectImage'),projectController.addProjects)
+//getuserprojects
+router.get('/user/all-projects',jwtMiddleware,projectController.allUserProjects)
+//getallprojects
+router.get('/projects/all',jwtMiddleware,projectController.getallProjects)
+//gethomeprojects
+router.get('/projects/home-projects',projectController.getHomeProjects)
+
 //export router
 module.exports = router
